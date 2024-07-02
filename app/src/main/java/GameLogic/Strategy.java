584e1515ca0e1bcabd0562409c01bbe98ebe035a
Tong <2917898087@qq.com>
2024-07-02 13:27:34 +0800

    test

---
+++ app/src/main/java/GameLogic/Strategy.java
@@ -0,0 +1,24 @@
+package GameLogic;
+
+import java.util.ArrayList;
+import java.lang.Override;
+
+public interface Strategy{
+    public abstract void playCard();
+
+}
+
+//两种机器人出牌策略
+class trategyfindBigerCards implements Strategy{
+    @java.lang.Override
+    public void playCard() {
+
+    }
+}
+
+class StrategyoutCardByItself implements Strategy{
+    @java.lang.Override
+    public void playCard() {
+
+    }
+}
